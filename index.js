8144a8e5aa9e87fbbf3b538eb54f90804f714b89
Veronica Celseti <veronicacelseti12@gmail.com>
2020-12-09 16:00:04 +0100

    get routes

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const express = require('express')
 const app = express()
 const toDosRoutes = require('./routes/todos')
+
+app.all('*', (req, res, next) => {
+    console.log('All middlewere')
+    next()
+})
+app.use('/todos', toDosRoutes)
+app.listen(3000, () => console.log('server on port 3000'))
--- routes/todos.js
+++ routes/todos.js
@@ -5,4 +5,8 @@ router.get('/', (req, res) => {
     res.send('todos')
 })
 
+router.get('/:id([0-9]+)', (req, res) => {
+    res.send('todo with id ' + req.params.id)
+})
+
 module.exports = router
