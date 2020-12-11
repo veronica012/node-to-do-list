eb4936659ba0d807dc5fda345a5c06f74a500317
Veronica Celseti <veronicacelseti12@gmail.com>
2020-12-11 09:37:08 +0100

    install mysql2

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const toDosRoutes = require('./routes/todos')
 const listsRoutes = require('./routes/lists')
+
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 app.use('/todos', toDosRoutes)
